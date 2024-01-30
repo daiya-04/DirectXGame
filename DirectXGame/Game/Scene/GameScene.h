65878e55f53b645c62af40de73e6d81007af92ab
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2024-01-30 16:54:21 +0900

    .h整理

--- DirectXGame/Game/Scene/GameScene.h
+++ DirectXGame/Game/Scene/GameScene.h
@@ -38,30 +38,6 @@ public:
 
 private:
 
-	Camera camera_;
-
-	Model* Model_ = 0;
-	Model* Model2_ = 0;
-	std::unique_ptr<Object3d> obj_;
-	std::unique_ptr<Object3d> obj2_;
-	WorldTransform objWT_;
-	WorldTransform objWT2_;
-
-	std::unique_ptr<Particle> particle_;
-	std::list<Particle::ParticleData> particleData_;
-	Particle::Emitter emitter_{};
-	const float kDeltaTime = 1.0f / 60.0f;
-
-	Model* terrainModel_ = 0;
-	std::unique_ptr<Object3d> terrain_;
-	WorldTransform terrainWT_;
-
-	PointLight pointLight_;
-	SpotLight spotLight_;
-
-	std::unique_ptr<LevelData> levelData_;
-	std::vector<std::unique_ptr<Object3d>> objects_;
-	std::vector<WorldTransform> WTs_;
-
+	
 };
 
