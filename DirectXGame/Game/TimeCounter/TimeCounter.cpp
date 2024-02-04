76fbcf68bcb7ec76739760370c40d73c9081fb00
kura495 <104417866+kura495@users.noreply.github.com>
2024-02-05 01:23:09 +0900

    Draw Timer

--- DirectXGame/Game/TimeCounter/TimeCounter.cpp
+++ DirectXGame/Game/TimeCounter/TimeCounter.cpp
@@ -3,31 +3,40 @@
 void TimeCounter::Init()
 {
 	uint32_t numberTex = TextureManager::Load("numbers.png");
-	Sprite* sprite = new Sprite(numberTex, { 640.0f,360.0f }, { 640.0f,64.0f },
-		0.0f, { 0.5f,0.5f }, { 0.2f,0.2f,0.2f,1.0f });
-	sprite->Initialize();
+	Sprite* spriteOne = new Sprite(numberTex, { 640.0f,360.0f }, { 64.0f,64.0f }, 0.0f, { 0.0f,0.5f }, { 0.2f,0.2f,0.2f,1.0f });
+	spriteOne->Initialize();
+	numbers_.push_back(std::unique_ptr<Sprite>(spriteOne));
 
-	numbers_.push_back(std::unique_ptr<Sprite>(sprite));
+	Sprite* spriteTwo = new Sprite(numberTex, { 640.0f,360.0f }, { 64.0f,64.0f }, 0.0f, { 1.0f,0.5f }, { 0.2f,0.2f,0.2f,1.0f });
+	spriteTwo->Initialize();
+	numbers_.push_back(std::unique_ptr<Sprite>(spriteTwo));
 }
 
 void TimeCounter::Update()
 {
 	if (IsTimeCount) {
-		count++;
+		flameCount++;
+
 	}
 #ifdef _DEBUG
 	ImGui::Begin("Timer");
-	ImGui::InputFloat("Count", &count);
-	ImGui::InputInt("Count", &number);
+	ImGui::InputFloat("Count", &flameCount);
+	ImGui::InputFloat("NumberCount", &NumberCount);
+	ImGui::InputFloat2("Anchor", &Anchor.x);
+	ImGui::InputFloat2("Pos", &Pos.x);
 	ImGui::End();
 #endif
-	number = (int)(count / 60);
+	NumberCount = flameCount / 60;
+	tenPlaceNumber = (int)NumberCount / 10;
+	onePlaceNumber = (int)NumberCount % 10;
 }
 
 void TimeCounter::Draw()
 {
 	//スプライトのn番を描画
-	for (std::unique_ptr<Sprite>& sprite : numbers_) {
-		sprite->Draw();
-	}
+	numbers_[ONEPLACE]->SetTextureArea(NumberValue[onePlaceNumber].Base, NumberValue[onePlaceNumber].Size);
+	numbers_[ONEPLACE]->Draw();
+	numbers_[TENPLACE]->SetTextureArea(NumberValue[tenPlaceNumber].Base, NumberValue[tenPlaceNumber].Size);
+	numbers_[TENPLACE]->Draw();
+	
 }
--- DirectXGame/Game/TimeCounter/TimeCounter.h
+++ DirectXGame/Game/TimeCounter/TimeCounter.h
@@ -3,7 +3,13 @@
 #include "TextureManager.h"
 #include "Sprite.h"
 
-#include <chrono>
+#define ONEPLACE 0
+#define TENPLACE 1
+
+struct NumberSprite {
+	Vector2 Base;
+	Vector2 Size;
+};
 
 class TimeCounter {
 public:
@@ -12,7 +18,7 @@ public:
 	void Update();
 	void Draw();
 
-	void Reset() { count = 0; };
+	void Reset() { flameCount = 0; };
 
 #pragma region
 	void IsTimerAnable() { IsTimeCount = true; }
@@ -22,9 +28,26 @@ public:
 private:
 	bool IsTimeCount = false;
 	
-	float count = 0;
-	int number;
+	float flameCount = 0;
+	float NumberCount = 0;
+	int onePlaceNumber = 0;
+	int tenPlaceNumber = 0;
+
+	Vector2 Anchor;
+	Vector2 Pos;
 
 	std::vector<std::unique_ptr<Sprite>>numbers_;
+	NumberSprite NumberValue[10] = {
+		{{0.0f,0.0f},{64.0f,64.0f}},	//0番
+		{{64.0f,0.0f},{64.0f,64.0f}},	//1番
+		{{128.0f,0.0f},{64.0f,64.0f}},	//2番
+		{{192.0f,0.0f},{64.0f,64.0f}},	//3番
+		{{256.0f,0.0f},{64.0f,64.0f}},	//4番
+		{{320.0f,0.0f},{64.0f,64.0f}},	//5番
+		{{384.0f,0.0f},{64.0f,64.0f}},	//6番
+		{{448.0f,0.0f},{64.0f,64.0f}},	//7番
+		{{512.0f,0.0f},{64.0f,64.0f}},	//8番
+		{{576.0f,0.0f},{64.0f,64.0f}},	//9番
+	};
 
 };
