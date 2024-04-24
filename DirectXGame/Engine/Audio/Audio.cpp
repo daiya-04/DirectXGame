8cef2d4f8299ec7449888875ab3ce4624091939f
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2024-04-24 14:09:01 +0900

    wave専用だった関数の削除

--- DirectXGame/Engine/Audio/Audio.cpp
+++ DirectXGame/Engine/Audio/Audio.cpp
@@ -27,10 +27,6 @@ Audio::~Audio() {
 	MFShutdown();
 }
 
-size_t Audio::LoadWave(const std::string& fileName) {
-	return Audio::GetInstance()->LoadWaveInternal(fileName);
-}
-
 size_t Audio::Load(const std::string& filename) {
 	return Audio::GetInstance()->LoadInternal(filename);
 }
@@ -63,7 +59,7 @@ void Audio::Update() {
 	}
 }
 
-size_t Audio::SoundPlayWave(size_t soundHandle, float volume, bool loop) {
+size_t Audio::Play(size_t soundHandle, float volume, bool loop) {
 	HRESULT hr;
 	const SoundData& soundData = soundData_.at(soundHandle);
 
@@ -95,36 +91,6 @@ size_t Audio::SoundPlayWave(size_t soundHandle, float volume, bool loop) {
 	sourceVoices_[playHandle]->SetVolume(volume);
 	return playHandle;
 }
-
-//size_t Audio::SoundPlayLoopStart(size_t soundHandle) {
-//	HRESULT hr;
-//	const SoundData& soundData = soundData_.at(soundHandle);
-//
-//	//再生する波形データの設定
-//	XAUDIO2_BUFFER buf{};
-//	buf.pAudioData = soundData.pBuffer.data();
-//	buf.AudioBytes = soundData.bufferSize;
-//	buf.Flags = XAUDIO2_END_OF_STREAM;
-//	
-//
-//	size_t playhandle = FindUnUsedPlayHandle();
-//	//プレイハンドルがいっぱい
-//	assert(playhandle < kMaxNumPlayHandles);
-//
-//	//sourceVoiceの作成
-//	IXAudio2SourceVoice* pSourcVoice = nullptr;
-//	hr = xAudio2_->CreateSourceVoice(&pSourcVoice, &soundData.wfex);
-//	assert(SUCCEEDED(hr));
-//
-//	hr = pSourcVoice->SubmitSourceBuffer(&buf);
-//	assert(SUCCEEDED(hr));
-//
-//	hr = pSourcVoice->Start();
-//	assert(SUCCEEDED(hr));
-//
-//	sourceVoices_[playhandle] = pSourcVoice;
-//	return playhandle;
-//}
 
 void Audio::SoundPlayLoopEnd(size_t playHandle) {
 	if (IsValidPlayhandle(playHandle)) {
@@ -192,113 +158,6 @@ size_t Audio::LoadInternal(const std::string& filename) {
 	soundData.wfex = *waveFormat;
 	soundData.bufferSize = sizeof(BYTE) * static_cast<uint32_t>(mediaData.size());
 	soundData.pBuffer = std::move(mediaData);
-	soundData_.emplace_back(soundData);
-
-	return soundData_.size() - 1;
-}
-
-size_t Audio::LoadWaveInternal(const std::string& filename) {
-	auto iter = std::find_if(soundData_.begin(), soundData_.end(), [&](const SoundData& soundData) {
-		return soundData.filename == filename;
-		});
-	if (iter != soundData_.end()) {
-		return std::distance(soundData_.begin(), iter);
-	}
-
-#pragma region ファイルオープン
-	//directoryPath
-	std::string directoryPath = "Resources/sounds/";
-	//ファイル出力ストリームのインスタンス
-	std::ifstream file;
-	//wavファイルをバイナリモードで開く
-	file.open(directoryPath + filename, std::ios_base::binary);
-	assert(file.is_open());
-
-#pragma endregion
-#pragma region wavデータの読み込み
-	//RIFFヘッダーの読み込み
-	RiffHeader riff;
-	file.read((char*)&riff, sizeof(riff));
-	//ファイルがRiffかチェック
-	if (strncmp(riff.chunk.id, "RIFF", 4) != 0) {
-		assert(0);
-	}
-	//タイプがWAVEかチェック
-	if (strncmp(riff.type, "WAVE", 4) != 0) {
-		assert(0);
-	}
-	//Formatチャンクの読み込み
-	FormatChunk format{};
-	//チャンクヘッダーの確認
-	file.read((char*)&format, sizeof(ChunkHeader));
-	if (strncmp(format.chunk.id, "fmt ", 4) != 0) {
-		assert(0);
-	}
-	//チャンク本体の読み込み
-	assert(format.chunk.size <= sizeof(format.fmt));
-	file.read((char*)&format.fmt, format.chunk.size);
-	//Dataチャンクの読み込み
-	ChunkHeader data;
-	file.read((char*)&data, sizeof(data));
-	//JUNKチャンクを検出した場合
-	if (strncmp(data.id, "JUNK", 4) == 0) {
-		//読み取りチャンクを検出した場合	
-		file.seekg(data.size, std::ios_base::cur);
-		//再読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	//LISTチャンクを検出した場合
-	if (strncmp(data.id, "LIST", 4) == 0) {
-		//読み取りチャンクを検出した場合	
-		file.seekg(data.size, std::ios_base::cur);
-		//再読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	// bextチャンクを検出した場合
-	if (strncmp(data.id, "bext", 4) == 0) {
-		// 読み取りチャンクを検出した場合
-		file.seekg(data.size, std::ios_base::cur);
-		// 再読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	//INFOチャンクを検出した場合
-	if (strncmp(data.id, "INFO", 4) == 0) {
-		// 読み取りチャンクを検出した場合
-		file.seekg(data.size, std::ios_base::cur);
-		// 再読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	//REAPERチャンクを検出した場合
-	if (strncmp(data.id, "REAPER", 6) == 0) {
-		// 読み取りチャンクを検出した場合
-		file.seekg(data.size, std::ios_base::cur);
-		// 再読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	if (strncmp(data.id, "junk", 4) == 0) {
-		// 読み取りチャンクを検出した場合
-		file.seekg(data.size, std::ios_base::cur);
-		// 再読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	if (strncmp(data.id, "data", 4) != 0) {
-		assert(0);
-	}
-	//Dataチャンクのデータ部(波形データ)の読み込み
-	std::vector<BYTE> pBuffer(data.size);
-	file.read(reinterpret_cast<char*>(pBuffer.data()), data.size);
-
-	//Waveファイルを閉じる
-	file.close();
-#pragma endregion
-#pragma region 読み込んだ音声データのreturn
-	//returnする為の音声データ
-	SoundData soundData{};
-	soundData.filename = filename;
-	soundData.wfex = format.fmt;
-	soundData.pBuffer = std::move(pBuffer);
-	soundData.bufferSize = data.size;
-#pragma endregion
 	soundData_.emplace_back(soundData);
 
 	return soundData_.size() - 1;
